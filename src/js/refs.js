83d15b4ba5a226f3324eef0302e9626dce30c0f9
JarPr <131611487+JarPr@users.noreply.github.com>
2023-10-04 17:22:55 +0200

    js-hw10

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export default {  
+ selectForm : document.querySelector('.breed-select'),
+ catInfo : document.querySelector('.cat-info'),
+ loadPage : document.querySelector('.loader'),
+ errorPage : document.querySelector('.error'),
+}
